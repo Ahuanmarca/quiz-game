cd4cf19624158b89b0549b67651ea9895fdb464a
Renzo Belon <renzobelon@gmail.com>
2023-11-25 18:25:04 +0100

    Questions never repeat

--- app.js
+++ app.js
@@ -55,25 +55,40 @@ function resetQuestion(question, gameState) {
   });
 }
 
-async function getQuestion(gameState) {
-  let level;
-  if (gameState.round <= 5) {
-    level = "easy";
-  } else if (gameState.roung <= 10) {
-    level = "medium";
-  } else {
-    level = "hard";
-  }
-  const category = ["javascript", "css", "html"][Math.floor(Math.random() * 3)];
+const getQuestion = (function () {
+  const usedQuestions = [];
 
-  // https://quiz-api-ofkh.onrender.com/questions/random?level=${level}&category=${category}
-  const response = await fetch(
-    `https://quiz-api-ofkh.onrender.com/questions/random?level=${level}&category=${category}`
-  );
-  const data = await response.json();
-  console.log(data.description);
-  Object.entries(data.answers).forEach((a) => console.log(a.join(": ")));
-  return data;
-}
+  async function getQuestion(gameState) {
+    let level;
+    if (gameState.round <= 5) {
+      level = "easy";
+    } else if (gameState.roung <= 10) {
+      level = "medium";
+    } else {
+      level = "hard";
+    }
+    const category = ["javascript", "css", "html"][
+      Math.floor(Math.random() * 3)
+    ];
+
+    const response = await fetch(
+      `https://quiz-api-ofkh.onrender.com/questions/random?level=${level}&category=${category}`
+    );
+    const data = await response.json();
+    console.log(data);
+    
+    if (usedQuestions.findIndex((e) => e === data._id) !== -1) {
+      console.log("Repeated question found, fetching again.")
+      return await getQuestion(gameState);
+    } else {
+      console.log(data.description);
+      Object.entries(data.answers).forEach((a) => console.log(a.join(": ")));
+      usedQuestions.push(data._id);
+      console.log("Used questions:", usedQuestions);
+      return data;
+    }
+  }
+  return getQuestion;
+})();
 
 runGame();
